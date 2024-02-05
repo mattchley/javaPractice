d5fcb09075e0e8caa5348832e80bee6820937161
mattchley <mattchley@gmail.com>
2024-02-05 15:48:46 -0800

    outlining completed

---
+++ currencyConverter/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // upon start up, text explaining the app to the user
+        // list the types to convert
+        System.out.println(
+                "Greetings! This is a basic currency converter project. It uses the freecurrencyapi. Currently it can convert the following currencies to any of these 33 currencies: AUD, BGN, BRL, CAD, CHF, CNY, CZK, DKK, EUR, GBP, HKD, HRK, HUF, IDR, ILS, INR, ISK, JPY, KRW, MXN, MYR, NZD,  NOK, PHP, PLN, RON, RUB, SEK, SGD, THB, TRY, USD, ZAR");
+    }
+
+}
