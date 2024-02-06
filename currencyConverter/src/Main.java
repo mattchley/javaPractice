bfbb6ff2313ccffdf21d184b40ac766d16a094fb
mattchley <mattchley@gmail.com>
2024-02-06 14:21:48 -0800

    full run through with USD and SEK as currency conversion. Only Happy route.

--- currencyConverter/src/Main.java
+++ currencyConverter/src/Main.java
@@ -4,57 +4,60 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+        // TODO abstract the logic to allow for easy reference when getting negative
+        // path
+        public static void main(String[] args) {
+                // upon start up, text explaining the app to the user
+                // list the types to convert
+                Scanner stringInput = new Scanner(System.in);
+                Scanner doubleInput = new Scanner(System.in);
+                String startingCurrency;
+                Double startingCurrencyAmount;
+                String confirmStartingCurrency;
+                String endingCurrency;
+                String confirmEndingCurrency;
+                String[] currencyArray = { "AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP",
+                                "HKD",
+                                "HRK", "HUF", "IDR", "ILS", "INR", "ISK", "JPY", "KRW", "MXN", "MYR", "NZD", "NOK",
+                                "PHP", "PLN", "RON",
+                                "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR" };
+                String currencies = Arrays.toString(currencyArray);
+                List<String> currencylist = new ArrayList<String>(Arrays.asList(currencyArray));
 
-    public static void main(String[] args) {
-        // upon start up, text explaining the app to the user
-        // list the types to convert
-        Scanner input = new Scanner(System.in);
-        String startingCurrency;
-        String startingCurrencyAmount;
-        String confirmStartingCurrency;
-        String endingCurrency;
-        String confirmEndingCurrency;
-        String[] listofCurrencies = { "AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD",
-                "HRK", "HUF", "IDR", "ILS", "INR", "ISK", "JPY", "KRW", "MXN", "MYR", "NZD", "NOK", "PHP", "PLN", "RON",
-                "RUB", "SEK", "SGD", "THB", "TRY", "USD", "ZAR" };
-        String currencies = Arrays.toString(listofCurrencies);
-
-        System.out.println(
-                "Greetings! This is a basic currency converter project. It uses the freecurrencyapi. Currently it can convert the following currencies to any of these 33 currencies: "
-                        + currencies);
-
-        System.out.println(
-                "Choose the starting currency: " + currencies);
-        startingCurrency = input.nextLine();
-        System.out.println(
-                "Choose the amount exp: 1.00");
-        startingCurrencyAmount = input.nextLine();
-        System.out.println("You entered " + startingCurrencyAmount + " " + startingCurrency
-                + ", is this correct?");
-        confirmStartingCurrency = input.nextLine();
-        if (confirmStartingCurrency.equals("yes")) {
-            List<String> currencylist = new ArrayList<String>(Arrays.asList(listofCurrencies));
-            currencylist.remove(startingCurrency);
-            System.out.println(
-                    "Choose the ending currency: " + Arrays.toString(currencylist.toArray()));
-            endingCurrency = input.nextLine();
-            System.out.println("You entered " + endingCurrency + ", is this correct?");
-            confirmEndingCurrency = input.nextLine();
-            if (confirmEndingCurrency.equals("yes") || confirmEndingCurrency.equals("y")) {
-                // TODO: Code to grab conversion to
-                double convertedAmount = Double.parseDouble(startingCurrencyAmount) * 10.61;
                 System.out.println(
-                        startingCurrencyAmount + " " + startingCurrency + "converted to " +
-                                endingCurrency + " is "
-                                + convertedAmount);
-                input.close();
+                                "Greetings! This is a basic currency converter. It uses the freecurrencyapi. Currently it can convert the 33 currencies: \nChoose the starting currency: "
+                                                + currencies);
 
-            } else {
-                // code to deal with negative path
-            }
-        } else {
-            // code to deal with negative path
+                startingCurrency = stringInput.nextLine();
+                System.out.println(
+                                "Choose the amount exp: 1.00");
+                startingCurrencyAmount = doubleInput.nextDouble();
+                System.out.println("You entered " + startingCurrencyAmount + " " + startingCurrency
+                                + ", is this correct? Y/N");
+                confirmStartingCurrency = stringInput.nextLine();
+                if (confirmStartingCurrency.equals("y") || confirmStartingCurrency.equals("Y")) {
+                        currencylist.remove(startingCurrency);
+                        System.out.println(
+                                        "Choose the ending currency: " + Arrays.toString(currencylist.toArray()));
+                        endingCurrency = stringInput.nextLine();
+                        System.out.println("You entered " + endingCurrency + ", is this correct? Y/N");
+                        confirmEndingCurrency = stringInput.nextLine();
+                        if (confirmEndingCurrency.equals("y") || confirmEndingCurrency.equals("Y")) {
+                                // TODO: Code to grab conversion to
+                                double convertedAmount = startingCurrencyAmount * 10.61;
+                                System.out.println(
+                                                startingCurrencyAmount + " " + startingCurrency + "converted to " +
+                                                                endingCurrency + " is "
+                                                                + convertedAmount);
+                                stringInput.close();
+                                doubleInput.close();
+
+                        } else {
+                                // code to deal with negative path
+                        }
+                } else {
+                        // code to deal with negative path
+                }
+
         }
-
-    }
 }
