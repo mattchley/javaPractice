785fe34b63709726460a21f33e300fa9a0d1802a
mattchley <mattchley@gmail.com>
2024-02-08 15:15:08 -0800

    base line structure of API.

--- currencyConverter/src/Main.java
+++ currencyConverter/src/Main.java
@@ -1,13 +1,25 @@
+import java.io.IOException;
+
 public class Main {
 
-        public static void main(String[] args) {
-                boolean continueApp = true;
-                Game application = new Game();
-                do {
-                        if (!application.runCurrencyCoverter()) {
-                                application.terminateApplication();
-                                continueApp = false;
-                        }
-                } while (continueApp);
+        public static void main(String[] args) throws IOException, InterruptedException {
+                CallApi test = new CallApi();
+
+                try {
+                        test.res();
+
+                } catch (Exception e) {
+                        System.err.println(e);
+                }
+                // boolean continueApp = true;
+                // Game application = new Game();
+                // do {
+                //         if (!application.runCurrencyCoverter()) {
+                //                 application.terminateApplication();
+                //                 continueApp = false;
+                //         }
+                // } while (continueApp);
+
         }
+
 }
